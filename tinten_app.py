144b5e949bfe5f8ad1a133337e10b766d306ccfd
Yannickhier <spiele.paduraru@gmail.com>
2025-07-28 03:31:10 +0200

    Update tinten_app.py

--- tinten_app.py
+++ tinten_app.py
@@ -7,8 +7,8 @@ def gewinnrechner(
     sternentinte_menge: int,
     traumtinte_preis: float,
     sternentinte_preis: float,
-    traumtinte_preis_vortag: float,
-    sternentinte_preis_vortag: float
+    traumtinte_preis_wunsch: float,
+    sternentinte_preis_wunsch: float
 ):
     # Rechenweg Traumtinte
     traumtinte_umsatz = traumtinte_menge * traumtinte_preis
@@ -18,12 +18,12 @@ def gewinnrechner(
     sternentinte_umsatz = sternentinte_menge * sternentinte_preis
     gewinn_sternentinte = sternentinte_umsatz - einkaufspreis
 
-    # Rechenweg mit Vortagespreisen
-    traumtinte_umsatz_vortag = traumtinte_menge * traumtinte_preis_vortag
-    gewinn_traumtinte_vortag = traumtinte_umsatz_vortag - einkaufspreis
+    # Rechenweg mit Wunschpreisen
+    traumtinte_umsatz_wunsch = traumtinte_menge * traumtinte_preis_wunsch
+    gewinn_traumtinte_wunsch = traumtinte_umsatz_wunsch - einkaufspreis
 
-    sternentinte_umsatz_vortag = sternentinte_menge * sternentinte_preis_vortag
-    gewinn_sternentinte_vortag = sternentinte_umsatz_vortag - einkaufspreis
+    sternentinte_umsatz_wunsch = sternentinte_menge * sternentinte_preis_wunsch
+    gewinn_sternentinte_wunsch = sternentinte_umsatz_wunsch - einkaufspreis
 
     # Ab welchem Traumtinte-Preis lohnt sich der Tausch nicht mehr
     break_even_traumtinte_preis = sternentinte_preis / 10
@@ -34,10 +34,10 @@ def gewinnrechner(
         "Gewinn mit direktem Verkauf von Traumtinte": round(gewinn_traumtinte, 2),
         "Sternentinte Umsatz": sternentinte_umsatz,
         "Gewinn bei Herstellung und Verkauf von Sternentinte": round(gewinn_sternentinte, 2),
-        "Traumtinte Umsatz (Vortag)": traumtinte_umsatz_vortag,
-        "Gewinn Traumtinte (Vortagspreis)": round(gewinn_traumtinte_vortag, 2),
-        "Sternentinte Umsatz (Vortag)": sternentinte_umsatz_vortag,
-        "Gewinn Sternentinte (Vortagspreis)": round(gewinn_sternentinte_vortag, 2),
+        "Traumtinte Umsatz (Wunschpreis)": traumtinte_umsatz_wunsch,
+        "Gewinn Traumtinte (Wunschpreis)": round(gewinn_traumtinte_wunsch, 2),
+        "Sternentinte Umsatz (Wunschpreis)": sternentinte_umsatz_wunsch,
+        "Gewinn Sternentinte (Wunschpreis)": round(gewinn_sternentinte_wunsch, 2),
         "Tausch lohnt sich, wenn Traumtinte unter": round(break_even_traumtinte_preis, 2)
     }
 
@@ -50,8 +50,8 @@ with st.form("input_form"):
     sternentinte_menge = st.number_input("Herstellbare Sternentinten", value=61, step=1)
     traumtinte_preis = st.number_input("Aktueller Preis Traumtinte", value=13.88)
     sternentinte_preis = st.number_input("Aktueller Preis Sternentinte", value=120.0)
-    traumtinte_preis_vortag = st.number_input("Vortagespreis Traumtinte", value=18.5)
-    sternentinte_preis_vortag = st.number_input("Vortagespreis Sternentinte", value=185.0)
+    traumtinte_preis_wunsch = st.number_input("Wunschpreis Traumtinte", value=18.5)
+    sternentinte_preis_wunsch = st.number_input("Wunschpreis Sternentinte", value=185.0)
     submitted = st.form_submit_button("Berechnen")
 
 if submitted:
@@ -61,8 +61,8 @@ if submitted:
         sternentinte_menge,
         traumtinte_preis,
         sternentinte_preis,
-        traumtinte_preis_vortag,
-        sternentinte_preis_vortag
+        traumtinte_preis_wunsch,
+        sternentinte_preis_wunsch
     )
 
     st.subheader("Rechenweg & Ergebnisse")
